3e03dc4a02cdf9590d94d5c5b1bbfec89fe7e2f7
Connor Douglas <cjdouglas96@gmail.com>
2018-09-30 17:23:08 -0600

    Basic scaffolding complete

--- assets/js/atl-ai.js
+++ assets/js/atl-ai.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
-  $(window).scroll(function(){
-    $('.show-on-scroll').each(function(i) {
-      let topOffset = 50;
-      var elemTop = $(this).offset().top;
-      var winBottom = $(window).scrollTop() + $(window).height();
-      if (winBottom > elemTop + topOffset) {
+                  checkHiddenElem();
+                  $(window).scroll(function(){
+                                   checkHiddenElem();
+                                   })
+                  })
+
+function checkHiddenElem() {
+  $('.show-on-scroll').each(function(i) {
+                            let topOffset = 100;
+                            var elemTop = $(this).offset().top;
+                            var winBottom = $(window).scrollTop() + $(window).height();
+                            if (winBottom > elemTop + topOffset) {
                               console.log("Fade");
-                              $(this).fadeIn();
-      }
-      })
-    })
-  })
+                              $(this).animate({'opacity':'1'}, 500);
+                            }
+                            })
+}
